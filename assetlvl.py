fa877487303dc88436a0385a940ce6d0a8afdafe
ChimeraBlack1 <christopherparke@zoho.com>
2019-09-12 14:58:02 -0400

    re-wrote the whole program backwards. instead of grabbing two serials from wells and comparing a test serial against it, we grab a test serial and look for it in wells, if it's not in wells, we look in DLL. works perfectly.

--- assetlvl.py
+++ assetlvl.py
@@ -7,7 +7,7 @@ loc = ("WellsPortfolio.xlsx")
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 
-#open wells workbook
+#open DLL workbook
 loc2 = ("DLLPortfolio.xlsx")
 wb2 = xlrd.open_workbook(loc2)
 sheet2 = wb2.sheet_by_index(0)
@@ -25,45 +25,52 @@ NewWorkbookName = "myNewWb.xls"
 # end of excel sheet (report for perry)
 endOfXLSheet = 3587
 endOfWells = 2152
+endOfDLL = 1380
 
 
-for x in range(1,endOfWells):
-
-  wellsSerial = sheet.cell_value(x,5)
-  wellsAssetPrice = sheet.cell_value(x,6)
-
+for x in range(1,endOfXLSheet):
+  # get serial to test
   try:
-    DLLAssetPrice = sheet2.cell_value(x,19)
-    DLLserial = sheet2.cell_value(x,22)
+    testSerial = sheet3.cell_value(x,10)
   except:
     continue
 
-  for y in range(1, endOfXLSheet):
-    testSerial = sheet3.cell_value(y,10)
-    
+  # look in the wells portfolio for the serial
+  for y in range(1, endOfWells):
+    try:
+      wellsSerial = sheet.cell_value(y,5)
+      wellsAssetPrice = sheet.cell_value(y,6)
+    except:
+      continue
+
     if testSerial == "":
       break
     if testSerial == wellsSerial:
       try:
-        worksheet.write(y,0, testSerial)
-        worksheet.write(y,1, wellsAssetPrice)
+        worksheet.write(x,0, testSerial)
+        worksheet.write(x,1, wellsAssetPrice)
       except:
         continue
+      break
+
+  # look in the DLL portfolio for the serial
+  for y in range(1, endOfDLL):
+    try:
+      DLLAssetPrice = sheet2.cell_value(y,19)
+      DLLserial = sheet2.cell_value(y,22)
+    except:
+      continue
+
+    if testSerial == "":
       break
     if testSerial == DLLserial:
       try:
-        worksheet.write(y,0, testSerial)
-        worksheet.write(y,1, DLLAssetPrice)
+        worksheet.write(x,0, testSerial)
+        worksheet.write(x,1, DLLAssetPrice)
       except:
         continue
       break
 
+
 workbook.save(NewWorkbookName)
 print("saved: " + str(NewWorkbookName))
-
-# Take assetLvlPrice from LeaseData
-# binary search for the serial number In SherpaReport
-# inject assetLvlPrice in asselLvlPrice of that row
-
-
-
